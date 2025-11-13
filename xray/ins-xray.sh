f81c4fe413da79db63753d39d5ed849823944bfe
gilper0x <gpairdrop@gmail.com>
2025-11-13 11:03:39 +0700

    Update ins-xray.sh

--- xray/ins-xray.sh
+++ xray/ins-xray.sh
@@ -367,7 +367,7 @@ systemctl start xray
 # nginx
 cat > /etc/nginx/conf.d/xray.conf <<'EOF'
 server {
-    listen 8080;
+    listen 127.0.0.1:8080;
     server_name _;
 
     location /vless-ws {
@@ -411,7 +411,7 @@ server {
 }
 
 server {
-    listen 4433 ssl http2;
+    listen 127.0.0.1:4433 ssl http2;
     server_name _;
 
     ssl_certificate /usr/local/etc/xray/xray.crt;
